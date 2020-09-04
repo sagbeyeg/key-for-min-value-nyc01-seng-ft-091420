2991046ed54b6b97f19c3cd55ea40ecfe5749952
Sisanwunmi Agbeyegbe <agbeyegbesisan@gmail.com>
2020-09-04 14:00:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,12 @@
 
 def key_for_min_value(name_hash)
   lowest_value = nil
-  name_hash.each do |k,v|
+  
+  name_hash.each do |k, v|
     if lowest_value == nil || v < lowest_value
     lowest_value = v 
     lowest_key = k
+    end
   end
   lowest_key
 end
